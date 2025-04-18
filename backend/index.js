84df5237389d4ed8f1b6ffc292128e6c0b2a24ac
Ketan Dudka <ketandudka566123@gmail.com>
2025-04-18 11:52:37 +0530

    PUSH

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const port = process.env.PORT || 4000;
+
+try {
+    app.listen(port, ()=>{
+        console.log(`server running on port ${port}`)
+    })
+} catch (error) {
+    
+}
